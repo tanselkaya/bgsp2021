cf2018caa22083f8eae0671b320db8720d3bcb48
Tansel Kaya <tanselkaya@yahoo.com>
2021-04-03 13:47:55 +0300

    First commit

---
+++ src/P2p/P2pNetworks.h
@@ -0,0 +1,11 @@
+// Copyright (c) 2011-2016 The Cryptonote developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#pragma once
+
+namespace CryptoNote
+{
+  const static boost::uuids::uuid CRYPTONOTE_NETWORK = { { 0x48, 0x45, 0x41, 0x52, 0x54, 0x42, 0x45, 0x41, 0x54, 0x1A, 0x00, 0x12, 0x10, 0x11, 0x01, 0x10 } };
+
+}
